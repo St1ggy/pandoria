07d5adecfe70c62cb782193f25048a51c33d804b
d.bragin <jekyll@live.ru>
2017-05-01 19:12:25 +0700

    fix empty

--- source/components/Map/Hex/InfoBox/InfoBox.jsx
+++ source/components/Map/Hex/InfoBox/InfoBox.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import types from '../../types.json';
 
 const InfoBox = ({info}) => {
-	console.log(info);
-	return <span className="info-box">({info.x}, {info.y})<br/>{types[info.type]}</span>;
+	if(info.type == 0) {
+		return <span className="info-box"></span>;
+	} else {
+		return <span className="info-box">({info.x}, {info.y})<br/>{types[info.type]}</span>;
+	}
 }
 
 export default InfoBox;
